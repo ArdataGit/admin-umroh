45d874079bafe7e39c5c63d8594bd8de87e5d02d
yudayuda78 <85638796+yudayuda78@users.noreply.github.com>
2026-02-28 11:38:48 +0700

    Feat : history action login logout

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\HistoryAction;
 
 class AuthController extends Controller
 {
@@ -22,6 +23,13 @@ class AuthController extends Controller
         if (Auth::attempt($credentials, $request->boolean('remember'))) {
             $request->session()->regenerate();
 
+            HistoryAction::create([
+                'user_id' => Auth::id(),
+                'menu' => 'Authentication',
+                'action' => 'Login',
+                'keterangan' => 'User berhasil login'
+            ]);
+
             return redirect()->intended(route('dashboard.index'));
         }
 
@@ -32,6 +40,15 @@ class AuthController extends Controller
 
     public function logout(Request $request)
     {
+        if (Auth::check()) {
+            HistoryAction::create([
+                'user_id' => Auth::id(),
+                'menu' => 'Authentication',
+                'action' => 'Logout',
+                'keterangan' => 'User berhasil logout'
+            ]);
+        }
+
         Auth::logout();
 
         $request->session()->invalidate();
